7baa36d1db93d1189b60a9471139b47a605f1cfd
ZAHID188 <zahidpranta5@gmail.com>
2021-05-06 13:49:14 +0600

    Areas in asp.net [march25 part-2 ==35:00]
    For larger project we will have a lot of controllers and we will have main controller for clean code we make areas and there we added our diffrent MVC .

---
+++ SEMYPROJECT/Areas/Area1st/Area1stAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SEMYPROJECT.Areas.Area1st
+{
+    public class Area1stAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Area1st";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Area1st_default",
+                "Area1st/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ SEMYPROJECT/Areas/Area1st/Controllers/TestinfoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SEMYPROJECT.Areas.Area1st.Controllers
+{
+    public class TestinfoController : Controller
+    {
+        // GET: Area1st/Testinfo
+        public ActionResult Index(string id)
+        {
+            return View();
+        }
+    }
+}
