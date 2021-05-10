48a3f65b387a9b142494d3ac790c629825b551fa
ZAHID188 <zahidpranta5@gmail.com>
2021-05-10 23:09:48 +0600

    public function for bundles
    we used public function and we used it in the bundles.config

--- SEMYPROJECT/App_Start/BundleConfig.cs
+++ SEMYPROJECT/App_Start/BundleConfig.cs
@@ -1,30 +1,42 @@
 ﻿using System.Web;
 using System.Web.Optimization;
+using SE2018.Public;
 
 namespace SEMYPROJECT
 {
     public class BundleConfig
     {
+
+        //alt+enter=
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle(VirtualPathStrings.jQuery).Include(
                         "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle(VirtualPathStrings.jQueryAjax).Include(
+                        "~/Scripts/jquery.unobtrusive*")); 
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            bundles.Add(new ScriptBundle(VirtualPathStrings.jQueryValidate).Include(
                         "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle(VirtualPathStrings.jQueryUI).Include(
+                "~/Scripts/jquery-ui-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            bundles.Add(new ScriptBundle(VirtualPathStrings.Modernizr).Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new ScriptBundle(VirtualPathStrings.Bootstrap).Include(
                       "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle(VirtualPathStrings.BootstrapCss).Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle(VirtualPathStrings.jQueryUICss).Include(
+                "~/Content/themes/base/all.css"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
---
+++ SEMYPROJECT/Public/VirtualPathStrings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SE2018.Public
+{
+    //static class just provide function, and it is  
+    // not designed for initialize objects!!
+    // Non-initializable--static class
+    public static class VirtualPathStrings
+    {
+        //static read-only property, only get {} provided!!
+        public static string jQuery
+        {
+            get => "~/bundles/jquery";
+        }
+        public static string jQueryAjax
+        {
+            get => "~/bundles/jquery/unobtrusive-ajax";
+        }
+        public static string jQueryValidate
+        {
+            get => "~/bundles/jquery/validate";
+        }
+        public static string jQueryUI
+        {
+            get => "~/bundles/jqueryui";
+        }
+        public static string Modernizr
+        {
+            get => "~/bundles/modernizr";
+        }
+        public static string Bootstrap
+        {
+            get => "~/bundles/bootstrap";
+        }
+        public static string BootstrapCss
+        {
+            get => "~/Content/css";
+        }
+        public static string jQueryUICss
+        {
+            get => "~/Content/themes/base/jquery-ui";
+        }
+    }
+}
