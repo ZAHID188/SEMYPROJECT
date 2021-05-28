f20261400bc513f9c5bb136ffa75e9cd4aac8876
ZAHID188 <zahidpranta5@gmail.com>
2021-05-29 01:04:43 +0600

    some correction

--- SEMYPROJECT/Models/City.cs
+++ SEMYPROJECT/Models/City.cs
@@ -19,14 +19,21 @@ namespace SEMYPROJECT.Models
         [Display(Name ="city name")]
         [MaxLength(20,ErrorMessage ="{0} name can't be more than{1}")]
         [Index("IX_city_cityName",Order =2,IsUnique =true)]
-        public string CityName { set;get }
+        public string CityName { set; get; }
         [Required]
         [Display (Name = "city name")]
         [MaxLength(15, ErrorMessage = "{0} name can't be more than{1}")]
         [Index("IX_city_cityName", Order = 1, IsUnique = true)]
-        [ForeignKey()]
+        //[ForeignKey("Province")]
         public string ProvinceID { set; get; }
-        // other need to be continued
 
+        //Navigation object: the Province whose ProvinceID is equal to...
+        //  virtual means "delay the loading of object['s data]"
+        //  late-binding / run-time binding, related to polymorphism!
+        [ForeignKey("ProvinceID")]
+        public virtual Province Province { set; get; }
+
+        //★★ Newly added: The counties in the current city:
+       // public virtual ICollection<County> Counties { set; get; }
     }
 }
